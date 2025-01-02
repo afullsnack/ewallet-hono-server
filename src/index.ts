e3f86dded6d3eee151c4e641342b4e17496f217b
Miracle Friday <miraclef60@gmail.com>
2025-01-03 00:15:26 +0100

    refactor: Refactor app setup controller to handle graceful shutdown of applicationg
    - Close server connections on terminal process close
    - Close server connections on `uncaughtException` error emitted

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,44 @@ import "dotenv/config";
 import { serve } from '@hono/node-server'
 import { setupRouter } from './router';
 import app from "./app";
+import { logger } from "./middlewares/logger";
 
 setupRouter(app);
 // TODO: read port from configuration with effect/docker-compose
 const port = 9001
-serve({
-  fetch: app.fetch,
-  port
-}, (info) => {
-  console.log(info.port, ":::App is running");
-})
+const server = serve(
+  {
+    fetch: app.fetch,
+    port
+  },
+  (info) => {
+    logger.info(`Server is ready on port [${info.port}], with address [${info.address}]`);
+  }
+)
 
 // NOTE: implement graceful shutdown on process kill signal
-// process.on('SIGINT', async () => {
-//   console.log("Graceful shutdown and cleanup");
-//   serverType.close((err) => console.log(`Error trying to close connection ${err}`));
-// });
+process.on('SIGINT', () => {
+  server.close((err) => {
+    if (err) {
+      logger.error(`❌ Error trying to close connection ${err}`);
+    } else {
+      logger.info(`✅ Server closed successfully`);
+    }
+  });
+});
+process.on('uncaughtException', (err) => {
+  // log the exception
+  logger.fatal(err, 'uncaught exception detected');
+  // shutdown the server gracefully
+  server.close((err) => {
+    logger.error(err, 'Error closing server');
+    process.exit(1); // then exit
+  });
+
+  // If a graceful shutdown is not achieved after 1 second,
+  // shut down the process completely
+  setTimeout(() => {
+    process.abort(); // exit immediately and generate a core dump file
+  }, 1000).unref()
+  process.exit(1);
+});
