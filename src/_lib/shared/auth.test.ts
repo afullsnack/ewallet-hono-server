30d9daa5c5b973ba52b792fbea25c44ce3d216b2
Miracle Friday <miraclef60@gmail.com>
2025-01-05 01:30:45 +0100

    refactor: Refactor auth.test.ts

--- src/_lib/shared/auth.test.ts
+++ src/_lib/shared/auth.test.ts
@@ -3,7 +3,7 @@ import { LogtoAuthAdapter } from "./auth";
 import { Context } from "hono";
 
 const context = {} as Context ;
-test('it should initialise LogtoServiceAdapter and get config', async () => {
+test('it should initialise LogtoServiceAdapter and get config', () => {
   const adapter = new LogtoAuthAdapter(context);
   const result = adapter.getConfig;
   console.log(result, 'logto-config');
