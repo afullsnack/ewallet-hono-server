6cc080e180570d451432948202c012e17f45e0ea
Miracle Friday <miraclef60@gmail.com>
2025-02-19 15:08:44 +0100

    refactor: Refactor schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,12 +1,17 @@
 import { relations } from "drizzle-orm";
-import { customType, pgTable, text, boolean } from "drizzle-orm/pg-core";
-import {v4 as uuidv4} from "uuid";
+import { customType, pgTable, text, boolean, json, timestamp, integer } from "drizzle-orm/pg-core";
+import { v4 as uuidv4 } from "uuid";
 
 // TODO: move to network domain
 const supportedNetworks = [
   'evm',
   'solana',
   'tron',
+] as const;
+const guardianRequestStatus = [
+  'accepted',
+  'canceled',
+  'pending',
 ] as const;
 const bytea = customType<{ data: Buffer; notNull: false; default: false }>({
   dataType() {
@@ -19,25 +24,47 @@ export const usersTable = pgTable("users_table", {
   id: tableId(),
   email: text().notNull().unique(),
   username: text().default('').unique(),
-  createAt: text('created_at').default(new Date().toISOString()),
+  createAt: timestamp('created_at', { mode: 'string', withTimezone: true }).defaultNow().notNull(),
   logtoUserId: text().notNull().unique(),
-  metadata: text(),
+  metadata: json(), // will hold a json array of custom networks and token data
   isFullyOnboarded: boolean().default(false),
 });
 
 export const accountTable = pgTable('wallets_table', {
   id: tableId(),
-  userId: text('user_id').notNull().references(() => usersTable.id),
-  networkType: text('network_type', { enum: supportedNetworks }).notNull(),
-  shareA: bytea('share_a'),
-  shareB: bytea('share_b'),
-  shareC: bytea('share_c'),
+  userId: text('user_id').notNull().references(() => usersTable.id, { onDelete: 'cascade' }),
+  network: text('network_type', { enum: supportedNetworks }).notNull(),
+  chainId: integer(),
+  address: text(),
+  shareA: bytea('share_a'), // our db only stores share_a
+  shareB: bytea('share_b'), // sits on users device
+  shareC: bytea('share_c'), // backup - QR Code or Guardian
+  isBackedUp: boolean().default(false),
+  // reference user as guardian, and set delete action to null when user is deleted
+  guardianId: text('guardian_id').references(() => usersTable.id, { onDelete: 'set null' }),
+  recoveryPassword: text(),
+  createdAt: timestamp('created_at', { mode: 'string', withTimezone: false }).defaultNow().notNull(),
+});
+
+// guard request
+export const guardRequestTable = pgTable('guard_request', {
+  id: tableId(),
+  guardianId: text('guardian_id').references(() => usersTable.id, { onDelete: 'set null' }),
+  requestorId: text('requestor_id').references(() => usersTable.id, { onDelete: 'set null' }),
+  status: text('status', { enum: guardianRequestStatus }).default('pending').notNull(),
+});
+
+export const recoveryRequestTable = pgTable('recovery_request', {
+  id: tableId(),
+  expiresIn: timestamp('expires_in', { mode: 'string', withTimezone: true }).notNull(),
+  createdAt: timestamp('created_at', { mode: 'string', withTimezone: false }).defaultNow().notNull(),
+  requestorId: text('requestor_id').references(() => usersTable.id, { onDelete: 'cascade' }),
 });
 
 
 // -------------- RELATIONSHIPS ----------------
 export const userRelations = relations(usersTable, ({ many }) => ({
-  accounts: many(accountTable)
+  accounts: many(accountTable),
 }));
 
 export const accountRelations = relations(accountTable, ({ one }) => ({
@@ -45,4 +72,23 @@ export const accountRelations = relations(accountTable, ({ one }) => ({
     fields: [accountTable.userId],
     references: [usersTable.id],
   })
-}))
+}));
+
+export const guardRequestRelations = relations(guardRequestTable, ({ one }) => ({
+  guardian: one(usersTable, {
+    fields: [guardRequestTable.guardianId],
+    references: [usersTable.id]
+  }),
+  requestor: one(usersTable, {
+    fields: [guardRequestTable.requestorId],
+    references: [usersTable.id],
+    relationName: 'requestor'
+  })
+}));
+
+export const recoveryRequestRelations = relations(recoveryRequestTable, ({ one }) => ({
+  requestor: one(usersTable, {
+    fields: [recoveryRequestTable.requestorId],
+    references: [usersTable.id]
+  })
+}));
