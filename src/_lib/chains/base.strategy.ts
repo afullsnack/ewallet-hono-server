5771987061e6becff30c4c89d7bd352ec7753e41
Miracle Friday <miraclef60@gmail.com>
2025-02-20 23:43:33 +0100

    refactor: Refactor base and evm strategy for account creation and send and receive transactions

--- src/_lib/chains/base.strategy.ts
+++ src/_lib/chains/base.strategy.ts
@@ -1,12 +1,14 @@
+import { HDAccount } from "../helpers/wallet";
 
+export interface AccountCreationResult {mnemonic: string; accounts: HDAccount[]};
 interface IBaseChainStrategy {
-  createAccount: (path: string) => Promise<void>;
+  createAccount: (path: string) => Promise<AccountCreationResult>;
   send: () => Promise<void>;
   receive: () => Promise<string>;
 }
 
 export abstract class BaseChainStrategy implements IBaseChainStrategy {
-  abstract createAccount(path: string): Promise<void>;
+  abstract createAccount(path: string): Promise<AccountCreationResult>;
   abstract send(): Promise<void>;
   abstract receive(): Promise<string>;
 }
---
+++ src/_lib/chains/evm.strategy.ts
@@ -0,0 +1,33 @@
+import {AccountCreationResult, BaseChainStrategy} from "./base.strategy";
+import {createHDAccounts} from "../helpers/wallet";
+
+
+export class EVMChainStrategy extends BaseChainStrategy {
+
+  private PATH = `m/44'/60'/0'/0/0` // use evm path
+
+  // remove parameter requirements if no use
+  async createAccount(_: string): Promise<AccountCreationResult> {
+      const {mnemonic, accounts} = await createHDAccounts({
+        numberOfAccounts: 1,
+        startIndex: 0,
+        basePath: this.PATH
+      });
+
+    return {
+      mnemonic,
+      accounts
+    }
+  }
+
+  // send logic for EVM chains, implement with viem
+  async send(): Promise<void> {
+      throw new Error('Not implemented');
+  }
+
+  // get address to receive from db
+  async receive(): Promise<string> {
+      throw new Error('Not implemented');
+  }
+  
+}
