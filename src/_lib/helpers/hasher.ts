9aca050e52799168eba38c828356d19049686b8b
Miracle Friday <miraclef60@gmail.com>
2025-02-23 17:43:21 +0100

    refactor: Remove unused commented out code

--- src/_lib/helpers/hasher.ts
+++ src/_lib/helpers/hasher.ts
@@ -1,32 +1,5 @@
-// import { randomBytes, pbkdf2Sync, createHash, createCipheriv, createDecipheriv, scryptSync } from 'node:crypto';
 import * as crypto from "crypto";
-const ENCRYPTION_KEY = '';
 
-export class Hasher {
-
-  // static encrypt(text: any) {
-  //   const iv = crypto.randomBytes(16);
-  //   const key = Buffer.from(ENCRYPTION_KEY);
-  //   const cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
-  //   let encrypted = cipher.update(text);
-  //   encrypted = Buffer.concat([encrypted, cipher.final()]);
-  //   return iv.toString("hex") + ":" + encrypted.toString("hex");
-  // }
-
-  // static decrypt(hash: string) {
-  //   const [ivHex, encryptedHex] = hash.split(":");
-    // const iv = Buffer.from(ivHex, "hex");
-  //   const encrypted = Buffer.from(encryptedHex, "hex");
-  //   const decipher = crypto.createDecipheriv(
-  //     "aes-256-cbc",
-  //     Buffer.from(ENCRYPTION_KEY).toString("utf8"),
-  //     iv,
-  //   );
-  //   let decrypted = decipher.update(encrypted);
-  //   decrypted = Buffer.concat([decrypted, decipher.final()]);
-  //   return decrypted.toString();
-  // }
-}
 export class CryptoUtil {
   // Using AES-256-GCM for authenticated encryption
   private static readonly ALGORITHM = 'aes-256-gcm';
