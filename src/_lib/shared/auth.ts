3c8506738a7015897122262ccd8dc4761b7894cc
Miracle Friday <miraclef60@gmail.com>
2025-03-04 12:33:41 +0100

    feat: setup better auth service

--- src/_lib/shared/auth.ts
+++ src/_lib/shared/auth.ts
@@ -4,78 +4,10 @@ import { pipe, Effect } from "effect";
 import { Schema } from "@effect/schema";
 import { HttpClient, HttpClientResponse, HttpClientRequest } from "@effect/platform"
 import { Context } from "hono";
+import {betterAuth} from "better-auth";
+import {drizzleAdapter} from "better-auth/adapters/drizzle";
+import {db} from "../../db"
 
-
-const TokenResponse = Schema.Struct({
-  access_token: Schema.String,
-  expires_in: Schema.Number,
-  token_type: Schema.String,
-  scope: Schema.String,
+export const auth = betterAuth({
+  database: drizzleAdapter(db, { provider: 'pg' })
 });
-
-interface ILogtoService {
-  register({
-    username,
-    email,
-    password,
-  }: { username: string; email: string; password: string; }): Promise<any>;
-  login({ email, password }: { email: string; password: string; }): Promise<any>
-}
-
-export class LogtoAuthAdapter implements ILogtoService {
-  private endpoint = process.env.LOGTO_APP_ENDPOINT!;
-  private appId = process.env.LOGTO_APP_ID!;
-  private appSecret = process.env.LOGTO_APP_SECRET!;
-  public auth = {};
-
-  constructor(c?: Context<Env>) { }
-
-  async generateAccessToken() {
-    return pipe(
-      HttpClientRequest.post(`${this.endpoint}/oidc/token`),
-      HttpClientRequest.acceptJson,
-      HttpClientRequest.setHeaders({
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': `Basic ${Buffer.from(`${this.appId}:${this.appSecret}`).toString('base64')}`
-      }),
-      HttpClientRequest.urlParamsBody({
-        'grant_type': 'client_credentials',
-        'resource': `${this.endpoint}/api`,
-        'scope': 'all'
-      }), // url params body
-      HttpClient.fetch,
-      Effect.tap((response) => Effect.gen(function*() {
-        const json = yield* response.json;
-        console.log(json, ":::json");
-      })),
-      Effect.andThen(HttpClientResponse.schemaBodyJson(TokenResponse, { errors: 'all' })),
-      Effect.scoped,
-      Effect.catchAll((e) => Effect.fail(e)),
-      Effect.andThen((value) => {
-        this.auth = {
-          token: value.access_token,
-          type: value.token_type,
-        };
-      }),
-      Effect.runPromise
-    )
-  }
-
-  async register({ username, email, password }: { username: string; email: string; password: string; }): Promise<{}> {
-    // handle register flow
-    logger.info(":register config", username, email, password);
-    return Promise.any([]);
-  }
-
-  async login({ email, password }: { email: string; password: string; }): Promise<any> {
-    logger.info(email, password);
-  }
-
-  get getConfig() {
-    return {
-      appId: this.appId,
-      appSecret: this.appSecret,
-      endpoint: this.endpoint,
-    };
-  }
-}
