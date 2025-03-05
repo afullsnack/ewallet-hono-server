1d3401a7ad42e9e2e2cc60bd600d732522fbbf33
Miracle Friday <miraclef60@gmail.com>
2025-03-05 22:16:18 +0100

    refactor: Add `/user/me` route to return user data with wallet if available

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,19 +1,32 @@
-import {HTTPException} from "hono/http-exception";
-import { logger } from "src/middlewares/logger";
+import { HTTPException } from "hono/http-exception";
+import { logger } from "../../middlewares/logger";
 import appFactory from "../../app";
-import {updateUserHandlers} from "./update";
+import { updateUserHandlers } from "./update";
+import { getUserWithWallets } from "../../db";
+import { auth } from "src/_lib/shared/auth";
 
 const userRoute = appFactory.createApp();
 
 userRoute.use(async (c, next) => {
   const session = c.get('session');
-  logger.info(session, ":::session");
   if(!session) {
     throw new HTTPException(401, {message: 'Unauthorized to access this route'});
   }
   await next();
 });
 
+// get user
+const getUserHandlers = appFactory.createHandlers(async (c) => {
+  const session = c.get('session');
+  if (!session) {
+    throw new HTTPException(404, { message: 'User not found' });
+  }
+  const fetchUser = await getUserWithWallets(session.userId)
+
+  return c.json(fetchUser, 200);
+})
+
 userRoute.post('/update', ...updateUserHandlers);
+userRoute.get('/me', ...getUserHandlers);
 
 export { userRoute };
