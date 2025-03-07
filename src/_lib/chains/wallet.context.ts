2b06bf440fa9227a6080ecd351d2e279892597fc
Miracle Friday <miraclef60@gmail.com>
2025-03-07 23:27:08 +0100

    refactor: Wrap async functions in cleaner tryCatch function

--- src/_lib/chains/wallet.context.ts
+++ src/_lib/chains/wallet.context.ts
@@ -1,13 +1,14 @@
 import { AccountCreationInput, AccountRecoveryInput, IBaseChainStrategy } from "./base.strategy";
 import { Network } from "../helpers/config";
 import { EVMChainStrategy } from "./evm.strategy";
+import { tryCatch } from "../try-catch";
 
 export class WalletContext {
   // map strategy to network:w
   private strategy: IBaseChainStrategy;
 
   constructor(private network: Network) {
-    switch(network) {
+    switch (network) {
       case 'evm':
         this.strategy = new EVMChainStrategy();
         break;
@@ -16,29 +17,25 @@ export class WalletContext {
     }
   }
 
-  async createAccount(params:AccountCreationInput) {
-    try {
-      const creationResult = await this.strategy.createAccount(params);
-      return creationResult;
-    }
-    catch(error: any) {
-      console.log(error, {action: 'create-account', network: this.network});
-      throw error;
-    }
-  }
+  async createAccount(params: AccountCreationInput) {
+    const { data: creationResult, error } = await tryCatch(
+      this.strategy.createAccount(params),
+      { action: 'create-account', network: this.network }
+    );
+    if (error) throw error;
 
+    return creationResult;
+  }
 
   async recoverAccount(params: AccountRecoveryInput) {
-    try {
-      const recoveryResult = await this.strategy.recoverAccount(params);
-      return recoveryResult;
-    }
-    catch(error: any) {
-      console.error(error, {action: 'recover-account', network: this.network});
-      throw error;
-    }
+    const { data: recoveryResult, error } = await tryCatch(
+      this.strategy.recoverAccount(params),
+      { action: 'recover-account', network: this.network }
+    );
+    if (error) throw error;
+
+    return recoveryResult;
   }
 
-
-  async useAccount() {} // TODO: for internal transactions that require the private key without user adding password
+  async useAccount() { } // TODO: for internal transactions that require the private key without user adding password
 }
