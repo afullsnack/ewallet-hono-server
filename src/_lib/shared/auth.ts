241c34ba128c9ecf2ecf0dc4e89c2aaefc4ed5d6
Miracle Friday <miraclef60@gmail.com>
2025-03-07 14:09:41 +0100

    refactor: Update base URL

--- src/_lib/shared/auth.ts
+++ src/_lib/shared/auth.ts
@@ -4,7 +4,7 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../../db"
 
 
-export const baseURL = `https://3efd-98-97-79-51.ngrok-free.app`;
+export const baseURL = `https://4076-2c0f-2a80-9a-2d10-b59c-78ad-66ec-e9f8.ngrok-free.app`;
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: 'pg' }),
   plugins: [
