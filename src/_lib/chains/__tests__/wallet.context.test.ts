4a5f13aac7d97d05a1774e70ba34ab024ab16323
Miracle Friday <miraclef60@gmail.com>
2025-03-07 23:29:35 +0100

    refactor: Update tests

--- src/_lib/chains/__tests__/wallet.context.test.ts
+++ src/_lib/chains/__tests__/wallet.context.test.ts
@@ -1,7 +1,7 @@
 import { describe, test, expect } from "vitest";
 import { WalletContext } from "../wallet.context";
 import { getWallet } from "../../../db";
-
+import * as ImageConverter from "image-conversion";
 
 const TEST_CONFIG = { timeout: 1000 * 4 };
 
@@ -9,10 +9,11 @@ const TEST_CONFIG = { timeout: 1000 * 4 };
 let mnemonic: string;
 let address: string;
 let privateKey: string;
+const userId = 'usr_sdonbasodvoboasv';
 
 test("should create evm accounts", async () => {
   const walletContext = new WalletContext('evm');
-  const account = await walletContext.createAccount({});
+  const account = await walletContext.createAccount({userId});
   console.log(account, ":::evm account created");
   mnemonic = account.mnemonic;
   address = account.accounts[0].address;
@@ -26,7 +27,7 @@ test("should create evm accounts", async () => {
 // test 3 - test account persistence with the same mnemonic
 test("should re-create the same account with the same mnemonic", async () => {
   const walletContext = new WalletContext('evm');
-  const account = await walletContext.createAccount({ mnemonic });
+  const account = await walletContext.createAccount({ mnemonic, userId });
   console.log(account, ":::evm account created");
 
   const newAddress = account.accounts[0].address;
@@ -47,6 +48,7 @@ test("should create keys, split to shares and save in db", async () => {
     const walletContext = new WalletContext("evm");
     const account = await walletContext.createAccount({
       password,
+      userId
     });
     console.log(account, ":::created account");
   }
@@ -57,7 +59,7 @@ test("should create keys, split to shares and save in db", async () => {
 }, TEST_CONFIG);
 
 
-test.only("should recover keys with with password and backup db", async () => {
+test("should recover keys with with password and backup db", async () => {
   try {
     const wallet = await getWallet('f006dfbd-fb54-4a19-8cbb-fd9e5611eb15');
     const password = 'kiloBYTE1234!';
