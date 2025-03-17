9e4e0fac5a31efe89e3e1d8d2b59b58f06656d02
Miracle Friday <miraclef60@gmail.com>
2025-03-17 22:01:36 +0100

    refactor: Expand guardian creation route to handle guardian flows

--- src/routes/guardians/create.ts
+++ src/routes/guardians/create.ts
@@ -4,4 +4,24 @@ import {z} from "zod"
 
 const guardian = appFactory.createApp();
 
+guardian.use(async (c, next) => {
+  // TODO: middleware to block or filter requests
+  await next();
+})
+
+guardian.post(
+  '/email',
+  zValidator('json', z.object({
+    email: z.string().email()
+  })),
+  async (c) => {
+    // TODO: confirm guardian or user email address
+
+    return c.json({
+      success: true,
+      message: 'Email confirmed!',
+    })
+  }
+)
+
 export { guardian };
