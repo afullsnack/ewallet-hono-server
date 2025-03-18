ed724e45c910f05c96736781e0aa3a2dd467f8d9
Miracle Friday <miraclef60@gmail.com>
2025-03-18 16:04:14 +0100

    feat: add route to get list of guardians for users

--- src/routes/guardians/index.ts
+++ src/routes/guardians/index.ts
@@ -1,8 +1,18 @@
+import { HTTPException } from "hono/http-exception";
 import appFactory from "../../app";
 import { guardian as guardianCreation } from "./create";
 import {guardianRecovery} from "./recover"
+import { db, getUserWithWallets } from "src/db";
 
 const guardianRoute = appFactory.createApp();
+
+// return list of guardians for user
+guardianRoute.get('/', async (c) => {
+  const user = c.get('user')
+  if(!user) throw new HTTPException(401, {message: 'You are not authorised to access this route'})
+
+  const userReq = await getUserWithWallets(user.id)
+})
 
 guardianRoute.route('/create', guardianCreation);
 guardianRoute.route('/recover', guardianRecovery);
