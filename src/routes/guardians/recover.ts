bf347a38d5b75bb0d1e20fce4095acb9b7825435
Miracle Friday <miraclef60@gmail.com>
2025-03-18 23:36:39 +0100

    refactor: Recover routes for guardian recovery

--- src/routes/guardians/recover.ts
+++ src/routes/guardians/recover.ts
@@ -1,8 +1,81 @@
 import appFactory from "../../app";
 import { zValidator } from "@hono/zod-validator"; 
+import { HTTPException } from "hono/http-exception";
+import { getUserWithEmailOrUsername } from "src/db";
 import {z} from "zod";
 
 const guardianRecovery = appFactory.createApp();
 
+
+// TODO: make routes for the recovery flow
+guardianRecovery.post(
+  '/send-code',
+  zValidator('json', z.object({
+    email: z.string().email()
+  })),
+  async (c) => {
+    const body = c.req.valid('json')
+
+    const user = await getUserWithEmailOrUsername(body.email)
+    if(!user) throw new HTTPException(404, {message: 'User not found with email'})
+
+    const response = {
+      success: true,
+      message: 'Verificatio code sent',
+      code: '000000'
+    }
+    return c.json(response)
+  }
+)
+
+guardianRecovery.post(
+  '/verify-code',
+  zValidator('json', z.object({
+    code: z.string().length(6)
+  })),
+  async (c) => {
+    const body = c.req.valid('json')
+
+    const response = {
+      success: true,
+      message: 'Code verified successful',
+    }
+    return c.json(response)
+  }
+)
+
+guardianRecovery.post(
+  '/verify-guardian-code',
+  zValidator('json', z.object({
+    guardCode: z.string().email()
+  })),
+  async (c) => {
+    const body = c.req.valid('json')
+
+    const response = {
+      success: true,
+      message: 'Verificatio code sent',
+      code: '000000'
+    }
+    return c.json(response)
+  }
+)
+
+guardianRecovery.post(
+  '/confirm-recovery',
+  // zValidator('json', z.object({
+  //   email: z.string().email()
+  // })),
+  async (c) => {
+    // const body = c.req.valid('json')
+
+    const response = {
+      success: true,
+      message: 'Verificatio code sent',
+      code: '000000'
+    }
+    return c.json(response)
+  }
+)
 
 export {guardianRecovery};
