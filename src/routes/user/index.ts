17c6087746ee7244cdd6366af95ca13a29b5ad0a
Miracle Friday <miraclef60@gmail.com>
2025-03-22 23:07:29 +0100

    refactor: Move balance route to wallet

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -30,40 +30,8 @@ const getUserHandlers = appFactory.createHandlers(async (c) => {
   return c.json({ user }, 200);
 })
 
-const getUserBalance = appFactory.createHandlers(async (c) => {
-  const session = c.get('session');
-  if (!session) {
-    throw new HTTPException(404, { message: 'User session not found' });
-  }
-  const user = await getUserWithWallets(session.userId)
-  if (!user) throw new HTTPException(404, { message: 'User was not found in db' });
-
-  let balance: number = 0;
-  if (user?.wallets.length) {
-    const { data, error } = await tryCatch(getBalance(84532, user?.wallets[0]?.address! as Address))
-    logger.error(error)
-    logger.info("balance:::", balance)
-    if(data) balance = data;
-  }
-
-  const usdBalance = balance*1963;
-  const assets = [
-    {
-        title: 'Base',
-        exchange: 0.08,
-        asset: balance,
-        slug: 'ETH',
-        pip: usdBalance,
-        signal: 0.31
-
-    }
-  ]
-
-  return c.json({ balance: usdBalance, assets, network: 'USD' }, 200)
-})
 
 userRoute.post('/update', ...updateUserHandlers);
 userRoute.get('/me', ...getUserHandlers);
-userRoute.get('/balance', ...getUserBalance)
 
 export { userRoute };
